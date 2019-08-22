c3804174a51bfee67f6900506d4546cd3baa5b03
marcinPluciennik <marcinpluciennik76@gmail.com>
2019-08-22 13:10:13 +0200

    hr model done

--- model/hr/hr.py
+++ model/hr/hr.py
@@ -12,7 +12,6 @@ from model import data_manager
 from model import common
 
 
-
 def add(table, record):
     """
     Add new record to table
@@ -25,6 +24,9 @@ def add(table, record):
         list: Table with a new record
     """
     # your code
+    data_manager.get_table_from_file(file_name)
+    table.append(record)
+    data_manager.write_table_to_file(file_name, table)
 
     return table
 
@@ -43,6 +45,12 @@ def remove(table, id_):
 
     # your code
 
+    data_manager.get_table_from_file(file_name)
+    for sublist in table:
+        if id_ in sublist:
+            table.remove(sublist)
+    data_manager.write_table_to_file(file_name, table)
+
     return table
 
 
@@ -60,8 +68,16 @@ def update(table, id_, record):
     """
 
     # your code
+    max_year = table[0][2]
 
-    return table
+    for sublist in table:
+        if sublist[2] < max_year:
+            max_year = sublist[2]
+
+    oldest_name = []
+    for sublist in table:
+        if max_year in sublist:
+            oldest_name.append(sublist[1])
 
 
 # special functions:
@@ -79,6 +95,18 @@ def get_oldest_person(table):
     """
 
     # your code
+    max_year = table[0][2]
+
+    for sublist in table:
+        if sublist[2] < max_year:
+            max_year = sublist[2]
+    print(max_year)
+
+    oldest_name = []
+    for sublist in table:
+        if max_year in sublist:
+            oldest_name.append(sublist[1])
+    print(oldest_name)
 
 
 def get_persons_closest_to_average(table):
@@ -93,3 +121,22 @@ def get_persons_closest_to_average(table):
     """
 
     # your code
+    year_avg = []
+    for sublist in table:
+        year_avg.append(int(sublist[-1]))
+
+    sum_year = 0
+    for element in year_avg:
+        sum_year += element
+
+    avg_year = int(sum_year / (len(year_avg)))
+
+    persons_closest_to_average_age = []
+
+    min = abs(avg_year - int(sublist[2]))
+    for sublist in table:
+        if abs(avg_year - int(sublist[2])) <= min:
+            min = abs(avg_year - int((sublist[2])))
+            persons_closest_to_average_age.append(sublist[1])
+    
+    return persons_closest_to_average_age
