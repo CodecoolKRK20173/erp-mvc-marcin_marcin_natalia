f7dea4eb0ab7b5333e36c8dd91a6f6382f529a65
NataliaMadry <nataliamadry321@gmail.com>
2019-08-22 13:15:35 +0200

    common controler

--- controller/common.py
+++ controller/common.py
@@ -1,3 +1,16 @@
 """ Common functions for controllers
 implement commonly used functions here
 """
+from model import data_manager
+
+
+def get_table(file_name):
+    return data_manager.get_table_from_file(file_name)
+
+
+def write_table(table, file_name):
+    data_manager.write_table_to_file(file_name, table)
+
+
+def find_id(table, index):
+    return table[index][0]
